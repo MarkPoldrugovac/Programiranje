9eed3dce5178b5a0ffb0213bcb06f040b34e979a
MarkPoldrugovac <poldrugovacmark01@gmail.com>
2025-10-08 09:50:39 +0200

    main.py

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
-#Funkcija za učitavanje teksta iz datoteke 
 def ucitaj_tekst(filepath):
     try:
-        # KOd za otvaranje datoteke ide ovdje
         with open (filepath, 'r', encoding='utf-8') as file:
             sadrzaj = file.read()
         return sadrzaj
     except FileNotFoundError:
         print(f"Greška: Datoteka na putanji '{filepath}' nije pronađena")
-        return None # Vratit ćemo 'ništa' ako datoteka ne postoji
+        return None 
       
-      # Funkcija za proišćavanje teksta 
 def ocisti_tekst(tekst):
-#Kod za pročišćavanje teksta ide ovdje
     tekst = tekst.lower()
-    interpunkcija = ['.',',','?',':',';','"',"'",'(',')']
+    interpunkcija = ['.', ',', '!', '?', ':', ';', '"', "'", '(', ')']
     for znak in interpunkcija:
-        tekst=tekst.replace(znak,'')
+        tekst = tekst.replace(znak, '')
 
     lista_rijeci = tekst.split()
-
+    
     return lista_rijeci
-       
+
+def broji_rijeci(lista_rijeci):
+    brojac_rijeci={}
+    for rijec in lista_rijeci:
+        if rijec in brojac_rijeci:
+            brojac_rijeci[rijec]+=1
+        else:
+            brojac_rijeci[rijec] =1
+    return brojac_rijeci
+
 if __name__=="__main__":
     filepath = "tekst.txt"
     print(f"Učitavam tekst iz datoteke: {filepath}")
@@ -30,9 +35,14 @@ if __name__=="__main__":
         print(ucitani_tekst)
     else:
         print("Greška pri učitavanju datoteke.")
+
     ucitani_tekst = ocisti_tekst(ucitani_tekst)
+
     if ucitani_tekst:
         print("Očišćeni tekst je:")
         print(ucitani_tekst)
+        brojac_rijeci=broji_rijeci(ucitani_tekst)
+        print("Broj riječui u tekstu:")
+        print(brojac_rijeci)
     else:
-        print("Greška pri očišćavanju teksta.")
+        print("Greška pri čišćenju teksta.")
